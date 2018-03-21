9d467fc9bdab847be2b29cb473d03d4f1b607722
xionghaiyu <xionghaiyu@carkusoft.com>
2018-03-21 17:51:20 +0800

    代码结构变化

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,11 @@
-let API_HOST = "http://xxx.com/xxx";
+let API_HOST = "";
 let DEBUG = true;//切换数据入口
 var Mock = require('mock.js')
+
+module.exports = {
+  getlist
+}
+
 function getlist(data = '', fn, method = "get", header = {}) {
   if (!DEBUG) {
     wx.request({
@@ -17,23 +22,10 @@ function getlist(data = '', fn, method = "get", header = {}) {
     var res = Mock.mock({
       'error_code': '',
       'error_msg': '',
-      'data|10': [{
-        'id|+1': 1,
-        'img': "@image('200x100', '#4A7BF7','#fff','pic')",
-        'title': '@ctitle(3,8)',
-        'city': "@county(true)",
-        'stock_num': '@integer(0,100)',//库存数量  
-        'marketing_start': '@datetime()',
-        'marketing_stop': '@now()',
-        'price': '@integer(100,2000)',//现价，单位：分  
-        'original_price': '@integer(100,3000)'
-      }]
+      'data': [{}]
     })
     // 输出结果
     // console.log(JSON.stringify(res, null, 2))
     fn(res);
   }
-}
-module.exports = {
-  getlist
 }
