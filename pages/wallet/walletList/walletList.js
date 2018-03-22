c65c978c77230faba4e32466ff57ddf4c56d9b46
wangtongbef <wangt173111@163.com>
2018-03-22 23:13:03 +0800

    walllist

--- pages/wallet/walletList/walletList.js
+++ pages/wallet/walletList/walletList.js
@@ -5,19 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-      detail:[
-        {type:'收入',time:'2018-01-30  13:45:59',money:'900'},
-        { type: '提现', time: '2018-01-19  08:16:36', money: '80' },
-        { type: '收入', time: '2018-01-10  15:24:03', money: '200' },
-        { type: '提现', time: '2017-12-19  16:20:09', money: '450' },
-      ]
+    wallList:[
+      {type:'收入',time:'2018-01-30  13:45:59',money:'900'},
+      { type: '提现', time: '2018-01-19  08:16:36', money: '80' },
+      { type: '收入', time: '2018-01-10  15:24:03', money: '200' },
+      { type: '提现', time: '2017-12-19  16:20:09', money: '450' },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this
+    wx.request({
+      url: 'http://dev2.lystrong.cn/api/weapp/v1/finance/trade',
+      method: 'POST',
+      data: {
+        page: 1,
+        token: 'abc123'
+      },
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          wallList: res.data.data.list
+        })
+        // var wallList = that.data.wallList
+        // for (var i = 0; i < wallList.length;i++){
+        //   if (item.type === 1){
+        //     wallList[i].amountStatus = true
+        //   } else if (item.type === 2){
+        //     wallList[i].amountStatus = false
+        //   }
+        // }
+        console.log(that.data.wallList)
+      }
+    })
   },
   linkDetail(){
     wx.navigateTo({
