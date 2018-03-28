e7f7eeb0a11e2b32c2775edfb2806601dc83a59d
xionghaiyu <xionghaiyu@carkusoft.com>
2018-03-28 16:06:12 +0800

    login end

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,121 +1,5 @@
 var getUser = require('../../utils/getUser.js');
 
-// Page({
-//   data:{
-//       nickName:'',
-//       avatarUrl:'',
-//       telephone:'',
-//       flag:false,
-//       phonePrompt:'',
-//       codePrompt:'',
-//       promptColor:'',
-//       btnBackground:'',
-//       codeText:'获取验证码',
-//       time:60,
-//       codeShow:false,
-//       codeDis:true,
-//       submitDis:true,
-//       codeT:'',
-//       submitBg:''
-//   },
-//   onLoad:function(){
-//     getUser()
-//   },
-//   //手机号码验证
-//   phoneVal:function(e){
-//       var that = this; 
-//       //console.log(e.detail.value);
-//       var telephone = e.detail.value;
-//       if (/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$/.test(telephone)){
-//         console.log('手机验证通过啦')
-//         that.setData({
-//           flag: true,
-//           phonePrompt: '输入正确',
-//           promptColor:'green',
-//           codeDis:false,
-//           btnBackground: 'linear-gradient(#018fe8,#0072b1)',
-//           telephone: telephone
-//         })
-//       }else{
-//         that.setData({
-//           flag:true,
-//           phonePrompt:'请输入正确的手机号码格式',
-//           promptColor: 'red',
-//           codeDis: true,
-//           btnBackground:'#C7C7C7',
-//           submitBg: '',
-//           submitDis: true
-//         })
-//       }
-//   },
-//   //发送验证码
-//   sendCode:function(){
-//     var code = (wx.getStorageSync('code'));
-//     var that = this;
-//     console.log(code)
-//     wx.request({
-//       url: 'http://dev2.lystrong.cn/api/weapp/v1/login/sendMsg/',
-//       method: 'POST',
-//       data:{
-//         phone:this.data.telephone, 
-//         token: 'abc'
-//       },
-//       success:function(e){
-//         console.log(e)
-//         that.setData({
-//           btnBackground: '#C7C7C7',
-//           codeShow: true,
-//           codeDis: true
-//         })
-//         var time = that.data.time;
-//         var timer = setInterval(function () {
-//           time--;
-//           that.setData({
-//             time: time
-//           })
-//           if(time === 0){
-//             clearInterval(timer)
-//             that.setData({
-//               time: 60,
-//               codeShow:false,
-//               codeDis:false,
-//               btnBackground: 'linear-gradient(#018fe8,#0072b1)'
-//             })
-//             return;
-//           }
-//         }, 1000)
-        
-//       },
-//       fail:function(e){
-//         console.log(e)
-//       }
-//     })
-//   },
-
-//   codeVal(e){
-//     var that = this;
-//     var value = e.detail.value;
-//     if(value.length > 5){
-//       that.setData({
-//         submitBg: 'linear-gradient(#018fe8,#0072b1)',
-//         submitDis:false
-//       })
-//     }else{
-//       that.setData({
-//         submitBg: '',
-//         submitDis: true
-//       })
-//     }
-//   },
-//   //登录
-//   navigatorBtn(e){
-//     console.log(e.detail.value)
-//     /*wx.navigateTo({
-//       url: '../index/index',
-//     })*/
-//   }
-// })
-
 Page({
   data:{
     token:'',
@@ -309,26 +193,20 @@ Page({
     }
   },*/
 
-  //登录
-  login(e){
-    var code = wx.getStorageSync('code');//code
-    console.log('code信息',code);
+  //登录并绑定手机号接口
+  login(){
     var that = this;
-    var phone = that.data.telephone
-    //console.log('电话号码',aa);
     wx.request({
-      url: getApp().data.servsers + 'login/login',
+      url: getApp().data.servsers + 'login/bind',
       method: 'POST',
       data:{
-        phone_number: phone,
+        phone_number: that.data.telephone,
         auth_code: that.data.codeVal,
-        wx_code: code
+        token: that.data.token
       },
-      success:function(e){
-        var statusCode = e.data.code
-        if (statusCode == 0){
-          console.log(e)
-          wx.setStorageSync('loginInfo', e.data.data);
+      success:function(res){
+        var code = res.data.code
+        if (code == 0){
           wx.showToast({
             title: '成功',
             icon: 'success',
@@ -336,13 +214,13 @@ Page({
           });
           var t = setTimeout(function(){
             wx.redirectTo({
-              url: '../rolesCheck/rolesCheck?token=' + e.data.data.token,
+              url: 'login',
             })
           },1000)
         }
-        if (statusCode == -1) {
+        if (statusCode !== 0) {
           wx.showToast({
-            title: '该手机已绑定其它微信,不可登录',
+            title: res.data.msg,
             icon: 'none',
             duration: 2000
           })
@@ -353,16 +231,5 @@ Page({
     /*wx.navigateTo({
       url: '../index/index',
     })*/
-  },
-  rolesCheck(token){
-    wx.request({
-      url: getApp().data.servsers + 'login/role',
-      data: {
-        token: token
-      },
-      success: function (res) {
-        console.log(res)
-      }
-    })
   }
 })
