bb43e24240e62418f8e1a60a290f68d7b6ad6764
wangtong <wangt173111@163.com>
2018-05-07 17:22:23 +0800

    token msg

--- pages/form/signForm/signForm.js
+++ pages/form/signForm/signForm.js
@@ -31,13 +31,13 @@ Page({
         wx.hideLoading()
         if (res.data.code == -3) {
           wx.showToast({
-            title: res.msg,
+            title: 'token过期',
             icon: 'none',
             duration: 1000
           })
           setTimeout(function () {
             wx.redirectTo({
-              url: '../login/login'
+              url: '../../login/login'
             })
           }, 1000)
         } else {
