fae9a2a7db6ff0974e9720c3fa0ac3ce46463e6e
wangtong <wangt173111@163.com>
2018-07-13 16:58:41 +0800

    senddetail

--- pages/goodsSend/goodssendDetail/goodssendDetail.js
+++ pages/goodsSend/goodssendDetail/goodssendDetail.js
@@ -5,14 +5,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    state:0,
+    role:'仓管员',
+    WHkeperType:'发货',
+    sendStep:'待取货',
+    sendType:'配送员'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
     console.log(options)
+    if (that.data.role == '仓管员'){
+      if (that.data.WHkeperType == '发货'){
+        if (that.data.sendStep == '待备货') {
+          that.setData({
+            state: 1,
+          })
+        } else if (that.data.sendStep == '待取货') {
+          if (that.data.sendType=='配送员'){
+            that.setData({
+              state: 2,
+            })
+          } else if (that.data.sendType == '物流'){
+            that.setData({
+              state: 3,
+            })
+          }
+        } else if (that.data.sendStep == '运送中'){
+          if (that.data.sendType == '物流'){
+            that.setData({
+              state: 2,
+            })
+          }
+        }
+      } else if (that.data.WHkeperType == '收货'){
+        if (that.data.sendStep == '运送中'){
+          that.setData({
+            state: 4,
+          })
+        }
+      }
+    } else if (that.data.role == '配送员'){
+      if (that.data.sendStep == '运送中'){
+        that.setData({
+          state: 2,
+        })
+      } else if (that.data.sendStep == '待取货'){
+        that.setData({
+          state: 4,
+        })
+      }
+    } else if (that.data.role == '门店负责人' || that.data.role == '门店销售员'){
+      if (that.data.sendStep == '运送中'){
+        that.setData({
+          state: 4,
+        })
+      }
+    }
   },
 
   /**
