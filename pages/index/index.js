33abb76803818be69896088499b48b5566a46bd7
wangtong <wangt173111@163.com>
2018-08-02 19:13:56 +0800

    send detail fixed

--- pages/index/index.js
+++ pages/index/index.js
@@ -40,13 +40,11 @@ Page({
     } else if (that.data.role.role_name == '门店负责人') {
       that.setData({
         list: [{ text: '门店', icon: '../../img/mendian_icon.png', url: '../store/storeList/storeList' },
-          { text: '售后扫码', icon: '../../img/saoyisao-icon.png', url: '../scanStorecheck/scanStorecheck' },
           { text: '报表', icon: '../../img/baobiao-icon.png', url: '../form/saleForm/saleForm' },
           { text: '资金', icon: '../../img/zijin-icon.png', url: '../wallet/walletCenter/walletCenter' }],
         List_storage: [{ text: '送货单', icon: '../../img/songhuodan-icon.png', url: '../goodsSend/goodssendList/goodssendList' },
           { text: '退货单', icon: '../../img/tuihuodan-icon.png', url: '../goodsReturn/goodsreturnList/goodsreturnList' },
-          { text: '存货', icon: '../../img/cunhuo-icon.png', url: '../Inventory/inventoryList/inventoryList' },
-          { text: '扫码看货', icon: '../../img/saomakanhuo-icon.png', url: '../scanGoods/scanGoods/scanGoods' }]
+          { text: '存货', icon: '../../img/cunhuo-icon.png', url: '../Inventory/inventoryList/inventoryList' }]
       })
     } else if (that.data.role.role_name == '商务专员') {
       that.setData({
